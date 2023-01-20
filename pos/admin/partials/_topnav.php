dbd6cd751d19ebadae4e69f746a0f8bc4508ce5f
Raymart <raymartformalejo00@gmail.com>
2023-01-20 14:32:06 +0800

    test
    title topnav

--- pos/admin/partials/_topnav.php
+++ pos/admin/partials/_topnav.php
@@ -5,44 +5,67 @@ $ret = "SELECT * FROM  rpos_admin  WHERE admin_id = '$admin_id'";
 $stmt = $mysqli->prepare($ret);
 $stmt->execute();
 $res = $stmt->get_result();
+
+function isHaveTextInUrl($text) {
+    return strpos($_SERVER['REQUEST_URI'], $text) !== false;
+  }
+
 while ($admin = $res->fetch_object()) {
 
 ?>
-    <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
-        <div class="container-fluid">
-            <!-- Brand -->
-            <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="dashboard.php"><?php echo $admin->admin_name; ?> Dashboard</a>
-            <!-- Form -->
+<nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
+  <div class="container-fluid">
+    <!-- Brand -->
+    <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="dashboard.php"><?php 
+       if(isHaveTextInUrl("dashboard")) {
+        echo $admin->admin_name. " " ."Dashboard";
+      } else if (isHaveTextInUrl("hrm")) {
+        echo "hrm";
+      }
+      else if (isHaveTextInUrl("customes")) {
+        echo "All customers";
+      } else if (isHaveTextInUrl("products")) {
+        echo "all products";
+      } else if (isHaveTextInUrl("orders")) {
+        echo "all orders";
+      } else if (isHaveTextInUrl("payments")) {
+        echo "payments";
+      } else if (isHaveTextInUrl("receipts")) {
+        echo "All receipts";
+      }
+      ?></a>
+    <!-- Form -->
 
-            <!-- User -->
-            <ul class="navbar-nav align-items-center d-none d-md-flex">
-                <li class="nav-item dropdown">
-                    <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <div class="media align-items-center">
-                            <span class="avatar avatar-sm rounded-circle">
-                                <img alt="Image placeholder" src="assets/img/theme/user-a-min.png">
-                            </span>
-                            <div class="media-body ml-2 d-none d-lg-block">
-                                <span class="mb-0 text-sm  font-weight-bold"><?php echo $admin->admin_name; ?></span>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
-                        <div class=" dropdown-header noti-title">
-                            <h6 class="text-overflow m-0">Welcome!</h6>
-                        </div>
-                        <a href="change_profile.php" class="dropdown-item">
-                            <i class="ni ni-single-02"></i>
-                            <span>My profile</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="logout.php" class="dropdown-item">
-                            <i class="ni ni-user-run"></i>
-                            <span>Logout</span>
-                        </a>
-                    </div>
-                </li>
-            </ul>
+    <!-- User -->
+    <ul class="navbar-nav align-items-center d-none d-md-flex">
+      <li class="nav-item dropdown">
+        <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+          aria-expanded="false">
+          <div class="media align-items-center">
+            <span class="avatar avatar-sm rounded-circle">
+              <img alt="Image placeholder" src="assets/img/theme/user-a-min.png">
+            </span>
+            <div class="media-body ml-2 d-none d-lg-block">
+              <span class="mb-0 text-sm  font-weight-bold"><?php echo $admin->admin_name; ?></span>
+            </div>
+          </div>
+        </a>
+        <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
+          <div class=" dropdown-header noti-title">
+            <h6 class="text-overflow m-0">Welcome!</h6>
+          </div>
+          <a href="change_profile.php" class="dropdown-item">
+            <i class="ni ni-single-02"></i>
+            <span>My profile</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="logout.php" class="dropdown-item">
+            <i class="ni ni-user-run"></i>
+            <span>Logout</span>
+          </a>
         </div>
-    </nav>
+      </li>
+    </ul>
+  </div>
+</nav>
 <?php } ?>
